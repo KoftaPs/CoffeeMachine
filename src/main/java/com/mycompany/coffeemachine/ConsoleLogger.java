0763888642bab2c97633e1fc914f92346ddd241b
KoftaPs <105562316+KoftaPs@users.noreply.github.com>
2023-05-18 04:26:06 +0300

    Create ConsoleLogger.java

---
+++ src/main/java/com/mycompany/coffeemachine/ConsoleLogger.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.coffeemachine;
+
+public class ConsoleLogger implements Logger {
+    @Override
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
